0511dc29a18f5fde38b3d6417bd3029c519e5149
Gary <p709723778@126.com>
2013-12-30 09:16:47 +0800

    Initial Commit

---
+++ Jccp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Jccp
+//
+//  Created by Gary on 13-12-30.
+//  Copyright (c) 2013年 蒲晓涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Jccp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Jccp
+//
+//  Created by Gary on 13-12-30.
+//  Copyright (c) 2013年 蒲晓涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
