031424cbbd5c420e7664e29ba75aff7e5660ad7e
Gary <Gary@GarydeMacBook-Pro.local>
2014-04-06 16:27:04 +0800

    设置主视图两边按钮

--- Jccp/Controller/GameHall/GYGameHallViewController.m
+++ Jccp/Controller/GameHall/GYGameHallViewController.m
@@ -8,6 +8,9 @@
 
 #import "GYGameHallViewController.h"
 
+#import "UIViewController+MMDrawerController.h"
+#import "MMDrawerBarButtonItem.h"
+
 @interface GYGameHallViewController ()
 
 @end
@@ -37,7 +40,60 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     self.title = @"购彩大厅";
+    
+    UITapGestureRecognizer * doubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(doubleTap:)];
+    [doubleTap setNumberOfTapsRequired:2];
+    [self.view addGestureRecognizer:doubleTap];
+    
+    UITapGestureRecognizer * twoFingerDoubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(twoFingerDoubleTap:)];
+    [twoFingerDoubleTap setNumberOfTapsRequired:2];
+    [twoFingerDoubleTap setNumberOfTouchesRequired:2];
+    [self.view addGestureRecognizer:twoFingerDoubleTap];
+    
+    
+    [self setupLeftMenuButton];
+    [self setupRightMenuButton];
+    
     [self.view setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
+}
+
+
+-(void)setupLeftMenuButton{
+    
+    UIButton *leftButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 22, 22)];
+    
+    [leftButton setBackgroundImage:[UIImage imageNamed:@"tab_icon1"] forState:UIControlStateNormal];
+    [leftButton setBackgroundImage:[UIImage imageNamed:@"tab_icon2"] forState:UIControlStateHighlighted];
+    [leftButton addTarget:self action:@selector(leftDrawerButtonPress:) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem *leftButtonItem = [[UIBarButtonItem alloc] initWithCustomView:leftButton];
+    [self.navigationItem setLeftBarButtonItem:leftButtonItem animated:YES];
+}
+
+-(void)setupRightMenuButton{
+    UIButton *rightButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 22, 22)];
+    
+    [rightButton setBackgroundImage:[UIImage imageNamed:@"tab_icon3"] forState:UIControlStateNormal];
+    [rightButton setBackgroundImage:[UIImage imageNamed:@"tab_icon4"] forState:UIControlStateHighlighted];
+    [rightButton addTarget:self action:@selector(rightDrawerButtonPress:) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem *rightButtonItem = [[UIBarButtonItem alloc] initWithCustomView:rightButton];
+    [self.navigationItem setRightBarButtonItem:rightButtonItem animated:YES];
+}
+
+#pragma mark - Button Handlers
+-(void)leftDrawerButtonPress:(id)sender{
+    [self.mm_drawerController toggleDrawerSide:MMDrawerSideLeft animated:YES completion:nil];
+}
+
+-(void)rightDrawerButtonPress:(id)sender{
+    [self.mm_drawerController toggleDrawerSide:MMDrawerSideRight animated:YES completion:nil];
+}
+
+-(void)doubleTap:(UITapGestureRecognizer*)gesture{
+    [self.mm_drawerController bouncePreviewForDrawerSide:MMDrawerSideLeft completion:nil];
+}
+
+-(void)twoFingerDoubleTap:(UITapGestureRecognizer*)gesture{
+    [self.mm_drawerController bouncePreviewForDrawerSide:MMDrawerSideRight completion:nil];
 }
 
 - (void)didReceiveMemoryWarning
